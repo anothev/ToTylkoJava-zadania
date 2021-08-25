571d34113e1b8477c66039b2a93088bf73da99d2
anothev <iza.szmiel@gmail.com>
2021-08-25 18:00:10 +0200

    wersja1

---
+++ src/Zadanie2.java
@@ -0,0 +1,20 @@
+public class Zadanie2 {
+    public static void main(String[] args) {
+        int stars = 1;
+
+
+        for (int y = 1; y <= 7; y++) {
+            System.out.print(y + ": ");
+
+            for (int space = 7; space > y; space--) {
+                System.out.print("-");
+            }
+            for (stars = 0; stars < y; stars++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
