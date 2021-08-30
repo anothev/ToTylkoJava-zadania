ef1071341ff08eaae51bb28c983bf55f2ca88f83
anothev <iza.szmiel@gmail.com>
2021-08-30 21:24:27 +0200

    scanner poprawka

--- src/Zadanie2Scanner.java
+++ src/Zadanie2Scanner.java
@@ -7,31 +7,29 @@ public class Zadanie2Scanner {
         System.out.println("Ile rzędów gwiazdek Ci wydrukowac: ");
         int rowCount = scanner.nextInt();
 
-        if (rowCount >= 1 && rowCount <= 20) {
-
-            for (int row = 0; row < rowCount; row++) {
-                int rowNo = row + 1;
-                System.out.format("%2d: ", rowNo);
-
-                int spacesCount = rowCount - row;
-                for (int space = 0; space < spacesCount; space++) {
-                    System.out.print(" ");
-                }
-
-                int starsCount = 2 * row + 1;
-                for (int stars = 0; stars < starsCount; stars++) {
-                    System.out.print("*");
-                }
-
-                System.out.println();
-            }
-        } else {
+        if (rowCount < 1 || rowCount > 20) {
             System.out.println("Liczba rzędów musi wynosic od 1 do 20");
+            return;
         }
 
+        for (int row = 0; row < rowCount; row++) {
+            int rowNo = row + 1;
+            System.out.format("%2d: ", rowNo);
+
+            int spacesCount = rowCount - row;
+            for (int space = 0; space < spacesCount; space++) {
+                System.out.print("-");
+            }
+
+            int starsCount = 2 * row + 1;
+            for (int stars = 0; stars < starsCount; stars++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
 
     }
-
 
 }
 
